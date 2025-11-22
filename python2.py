f163b7e64e5dd731604d19ec8ef46d47ed4bf48c
Arucha <craziie0023@gmail.com>
2025-11-22 14:09:49 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,52 @@
+"""
+#
+#Prrt: python operators
+#Arithemetic Operators
+#
+"""
+x= 4+5
+y=10-2
+z=3*7
+a=8/2
+b=10%3
+
+"""
+#
+# Part: Python Operators
+# Assignment Operators
+#
+"""
+
+x=5
+x= x + 3
+x += 3
+
+"""
+#
+# part:python operators
+# Comparison Operators
+#
+"""
+x=5
+y=10
+print(x==y) #false
+print(x!=y) #true
+print(x>y)  #false   
+print(x<y)  #true
+print(x>=y) #false
+print(x<=y) #true
+
+"""
+#Part: python Operators
+#Logical Operators
+#
+#
+"""
+x=5
+y=10
+print(x<10 and y>5) #true
+print(x<10 or y<5)  #true
+print(not(x < 10 )) #false
+print((x < 10) & (y > 5 )) #true
+print((x < 10) | (y < 5 ))  #true
+
