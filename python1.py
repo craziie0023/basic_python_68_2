18fa047529966f32c1f09a311dab39e393428f34
Arucha <craziie0023@gmail.com>
2025-11-22 13:24:16 +0700

    Variable

---
+++ python1.py
@@ -0,0 +1,43 @@
+"""
+#
+#Part: python comment
+#
+"""
+
+#
+print("hello world")
+#
+
+
+x=5 # Integer variable
+y="5" # String variable
+print(x,y)
+
+x=str(5)
+y=int(10)
+z=float(15)
+print(type(x),type(y),type(z))
+
+"""
+#
+# part: python Naming
+# 
+"""
+myvar = "John"
+my_var = "John"
+_my_var = "John"  
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+# 2my_var = "John"  # Invalid variable name
+# my-var = "John"  # Invalid variable name
+# my var = "John"  # Invalid variable name
+
+# camel Case
+myVariableName = "John"
+#pascal Case
+MyVariableName = "John"
+#snake Case
+my_variable_name = "John"
+
+
